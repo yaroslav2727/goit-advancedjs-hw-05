09cb95b6429e20cabb090d7360e38e3f80bad459
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-11-28 20:46:03 +0200

    livepage-fix

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
-import { concatenation } from './concatenation';
-const button = document.querySelector('button');
-const input = document.querySelector('input');
+import { concatenation } from "./concatenation.js";
+const button = document.querySelector("button");
+const input = document.querySelector("input");
 if (button && input) {
-    button.addEventListener('click', () => {
-        concatenation(input.value, 'hello!');
-    });
+  button.addEventListener("click", () => {
+    concatenation(input.value, "hello!");
+  });
 }
 //# sourceMappingURL=index.js.map
